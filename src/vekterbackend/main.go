b9d4fab3e29bfea5f11b62b5ae2ba3135ee4df46
Bjørnar Hammerseth <bjornar.hammerseth@gmail.com>
2023-03-28 17:08:21 +0200

    CORS changes

--- src/vekterbackend/main.go
+++ src/vekterbackend/main.go
@@ -5,6 +5,7 @@ import (
 	"VekterBackend/src/initializers"
 	"VekterBackend/src/middleware"
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +16,15 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}                                       // Adjust this to specify the allowed origins
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"} // Allowed HTTP methods
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
+
+	// Use the CORS middleware
+	r.Use(cors.New(config))
+
 	r.POST("/register", api.CreateUser)
 	r.POST("/login", api.Login)
 
